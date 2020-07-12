ec713e88fdbe6e64aa178cc513f9f54266411663
Jan Babušík <qwak623@gmail.com>
2020-07-12 22:25:22 +0200

    Add project files.

---
+++ AzureApp2020/Default.aspx.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AzureApp2020
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            MyLabel.Text = "Hello World from Azure";
+        }
+
+        protected void GoButton_Click(object sender, EventArgs e)
+        {
+            if (MyFileUpload.HasFile)
+            {
+                var storageAccount = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["StorageAccountConnectionString"].ConnectionString);
+                var blobClient = storageAccount.CreateCloudBlobClient();
+                var containerReference = blobClient.GetContainerReference("test"); // name of your container
+
+                var blobReference = containerReference.GetBlockBlobReference(MyFileUpload.FileName);
+                blobReference.UploadFromStream(MyFileUpload.FileContent);
+            }
+        }
+
+        protected override void OnPreRender(EventArgs e)
+        {
+            var storageAccount = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["StorageAccountConnectionString"].ConnectionString);
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            var containerReference = blobClient.GetContainerReference("test");
+
+            var blobs = containerReference.ListBlobs();
+            FilesRepeater.DataSource = blobs;
+            FilesRepeater.DataBind();
+        }
+
+        protected void FileLink_Command(object sender, CommandEventArgs e)
+        {
+            var storageAccount = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["StorageAccountConnectionString"].ConnectionString);
+
+            var blobClient = storageAccount.CreateCloudBlobClient();
+
+            var blobReference = new CloudBlockBlob(new Uri((string)e.CommandArgument), blobClient);
+            blobReference.DownloadToStream(Response.OutputStream);
+        }
+    }
+}
---
+++ AzureApp2020/Global.asax.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace AzureApp2020
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            string instrumentationKey = (string)System.Configuration.ConfigurationManager.AppSettings["ApplicationInsights"];
+            if (!string.IsNullOrEmpty(instrumentationKey))
+                Microsoft.ApplicationInsights.Extensibility.TelemetryConfiguration.Active.InstrumentationKey = instrumentationKey;
+        }
+    }
+}
